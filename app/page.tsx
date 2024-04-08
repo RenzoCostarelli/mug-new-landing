860fb85695116dc585eaa4cecab4aa75e9dbf2a0
Renzo Costarelli <renzo.costarelli@gmail.com>
2024-04-08 19:17:39 -0300

    next upgrade

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,3 @@
-"use client";
 import s from "./page.module.scss";
 import Hero from "./components/hero";
 import SnapScroller from "./components/snap-scroller";
@@ -8,33 +7,33 @@ import UI from "./components/ui";
 import { useEffect, useState } from "react";
 import FooterSection from "./components/footer-section";
 export default function Home() {
-  const [title1, setTitle1] = useState<string>("La web del Mug");
-  useEffect(() => {
-    let observers: IntersectionObserver[] = [];
+  // const [title1, setTitle1] = useState<string>("La web del Mug");
+  // useEffect(() => {
+  //   let observers: IntersectionObserver[] = [];
 
-    const sections = document.querySelectorAll("[data-title]");
-    sections.forEach((section) => {
-      const observer = new IntersectionObserver(
-        (entries) => {
-          entries.forEach((entry) => {
-            if (entry.isIntersecting) {
-              setTitle1(section.getAttribute("data-title") || "");
-            }
-          });
-        },
-        { threshold: 0.15 }
-      );
-      observer.observe(section);
-      observers.push(observer);
-    });
+  //   const sections = document.querySelectorAll("[data-title]");
+  //   sections.forEach((section) => {
+  //     const observer = new IntersectionObserver(
+  //       (entries) => {
+  //         entries.forEach((entry) => {
+  //           if (entry.isIntersecting) {
+  //             setTitle1(section.getAttribute("data-title") || "");
+  //           }
+  //         });
+  //       },
+  //       { threshold: 0.15 }
+  //     );
+  //     observer.observe(section);
+  //     observers.push(observer);
+  //   });
 
-    return () => {
-      observers.forEach((observer) => observer.disconnect());
-    };
-  }, []);
+  //   return () => {
+  //     observers.forEach((observer) => observer.disconnect());
+  //   };
+  // }, []);
   return (
     <>
-      <UI text1={title1} text2="Lorem ipsum" text3="lorem ipsum" />
+      <UI text1={"title1"} text2="Lorem ipsum" text3="lorem ipsum" />
       <header
         className={s.header_container}
         id="header"
